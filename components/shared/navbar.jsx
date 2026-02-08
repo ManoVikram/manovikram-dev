98d9c2b6c133372b87ecf0203490d012b8475219
Mano Vikram <manovik18@gmail.com>
2026-02-08 20:36:54 +0530

    Removed the unnecessary div and moved the logo 'mano' inside Link tag

--- components/shared/navbar.jsx
+++ components/shared/navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className=''>
             <div className="flex flex-row justify-between items-center">
-                <p className='font-quicksand font-bold text-2xl'>mano</p>
+                <Link href="/" className='font-quicksand font-bold text-2xl'>mano</Link>
 
                 <ul className='hidden md:flex md:flex-row md:justify-start md:items-center md:gap-8'>
                     <li>
@@ -15,10 +15,8 @@ const Navbar = () => {
                         <Link href='#about'>ABOUT</Link>
                     </li>
                     <li>
-                        <Link href='#contact'>
-                            <div className="flex justify-center items-center bg-primary text-white px-5 py-3 rounded-full">
-                                WORK WITH ME
-                            </div>
+                        <Link href='#contact' className="flex justify-center items-center bg-primary text-white px-5 py-3 rounded-full">
+                            WORK WITH ME
                         </Link>
                     </li>
                 </ul>
