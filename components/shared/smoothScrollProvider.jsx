23ac3115eba99a4aeafbf020f778aa0c95de2ecf
Mano Vikram <manovik18@gmail.com>
2026-02-14 00:54:14 +0530

    Updated the smooth scroll provider logic

--- components/shared/smoothScrollProvider.jsx
+++ components/shared/smoothScrollProvider.jsx
@@ -1,21 +1,36 @@
 "use client"
 
-import React from 'react'
-import { ReactLenis, useLenis } from 'lenis/dist/lenis-react'
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { ReactLenis } from "lenis/react"
+import { useEffect, useRef } from "react"
 
-const SmoothScrollProvider = ({ children }) => {
+export default function SmoothScrollProvider({ children }) {
+    const lenisRef = useRef()
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger)
+
+        const lenis = lenisRef.current?.lenis
+        if (!lenis) return
+
+        lenis.on("scroll", ScrollTrigger.update)
+
+        return () => {
+            lenis.off("scroll", ScrollTrigger.update)
+        }
+    }, [])
+
     return (
-        <ReactLenis root options={{
-            lerp: 0.1,
-            smoothWheel: true,
-            anchors: {
-                offset: 80
-            },
-            stopInertiaOnNavigate: true
-        }}>
+        <ReactLenis
+            ref={lenisRef}
+            root
+            options={{
+                lerp: 0.08,
+                smoothWheel: true,
+            }}
+        >
             {children}
         </ReactLenis>
     )
 }
-
-export default SmoothScrollProvider
