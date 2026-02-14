00b0e34076c4250a6a04224de2573449607d8dc8
Mano Vikram <manovik18@gmail.com>
2026-02-14 15:54:39 +0530

    Added animation to the hero component

--- components/sections/hero.jsx
+++ components/sections/hero.jsx
@@ -1,20 +1,47 @@
 "use client"
 
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 import Link from 'next/link'
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Hero = () => {
+    const heroContainerRef = useRef(null)
+
+    useGSAP(() => {
+        const tl = gsap.timeline({
+            defaults: {
+                duration: 1,
+                ease: "power3.out"
+            }
+        })
+
+        tl.from(".animate-p", {
+            x: 100,
+            opacity: 0,
+        }).from(".animate-title", {
+            x: 100,
+            opacity: 0,
+        }, "-=0.8").from(".animate-subtitle", {
+            x: 100,
+            opacity: 0,
+        }, "-=0.8").from(".animate-cta-button", {
+            x: 100,
+            opacity: 0
+        }, "-=1")
+    }, { scope: heroContainerRef })
+
     return (
-        <section className="min-h-dvh flex flex-col justify-center items-start py-6 md:py-8 gap-18">
-            <p className="">Software Engineer <span>·</span> Applied AI</p>
+        <section ref={heroContainerRef} className="min-h-dvh flex flex-col justify-center items-start py-6 md:py-8 gap-18">
+            <p className="animate-p">Software Engineer <span>·</span> Applied AI</p>
 
             <div className="flex flex-col gap-2 md:gap-4">
-                <h1 className="text-6xl md:text-7xl lg:text-8xl">Building software that thinks</h1>
+                <h1 className="animate-title text-6xl md:text-7xl lg:text-8xl">Building software that thinks</h1>
 
-                <p className="text-xl md:text-2xl text-secondary">Crafting products where AI feels like magic and software feels like second nature. I turn ambitious visions into daily rituals.</p>
+                <p className="animate-subtitle text-xl md:text-2xl text-secondary">Crafting products where AI feels like magic and software feels like second nature. I turn ambitious visions into daily rituals.</p>
             </div>
 
-            <Link href='#contact' className='group flex justify-between items-center bg-primary hover:bg-background border-[1] hover:border-[1] border-transparent hover:border-primary px-6 py-4 rounded-full gap-3 transition-colors duration-300 cursor-pointer'>
+            <Link href='#contact' className='animate-cta-button group flex justify-between items-center bg-primary hover:bg-background border-[1] hover:border-[1] border-transparent hover:border-primary px-6 py-4 rounded-full gap-3 transition-colors duration-300 cursor-pointer'>
                 <p className="text-white group-hover:text-primary">LET&apos;S BUILD TOGETHER</p>
 
                 <svg width="28" height="28" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" className='group-hover:rotate-45 transition-all duration-300'>
